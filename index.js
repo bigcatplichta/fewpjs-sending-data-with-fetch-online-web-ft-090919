6d08ae8bb5581f33b576cf1fdd168948ee20864e
Matt Plichta <bigcatplichta@gmail.com>
2020-01-07 03:14:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+      };
+    
+    const configObj = {
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+       
+    let data = fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            console.log(object);
+        })
+        .catch(function(error) {
+            alert("Bad things! Ragnarők!");
+            console.log(error.message);
+        });
+        
+    return data
+}
